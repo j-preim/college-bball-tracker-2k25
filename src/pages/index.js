c8f913d1d804833d55c3ed2f0e77c3b4e317c8d5
j-preim <j.preimesberger@gmail.com>
2025-03-16 22:12:35 -0500

    added pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+
+export { default as Home } from "./Home"
+export { default as Bracket } from "./Bracket"
+export { default as Matchups } from "./Matchups"
+export { default as Entries } from "./Entries"
+export { default as Auth } from "./Auth"
