4b643ea8718167d5930fe11291fdcd5a91b568b5
j-preim <j.preimesberger@gmail.com>
2025-03-16 21:04:26 -0500

    back to square 1

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const entryRoutes = require("./entry.routes");
-const userRoutes = require("./user.routes");
-
-router.use("/entry", entryRoutes);
-router.use("/user", userRoutes);
-
-module.exports = router;
