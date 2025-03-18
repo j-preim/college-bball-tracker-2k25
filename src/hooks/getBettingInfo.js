262b01053c44471909e996095c9991527783ab0d
j-preim <j.preimesberger@gmail.com>
2025-03-17 19:39:53 -0500

    TBD spread

--- src/hooks/getBettingInfo.js
+++ src/hooks/getBettingInfo.js
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react";
 import initBettingLinesDb from "../../public/initBettingLines.json";
 
 export const getBettingInfo = (gameId) => {
-  let bettingInfo;
+  let bettingInfo = "TBD";
   let finalBettingInfo;
 
   for (let i = 0; i < initBettingLinesDb.sport_events.length; i++) {
     if (initBettingLinesDb.sport_events[i].uuids === gameId) {
       bettingInfo = initBettingLinesDb.sport_events[i].consensus.lines[2].spread;
     }
-    else bettingInfo = "TBD"
   }
 
   
