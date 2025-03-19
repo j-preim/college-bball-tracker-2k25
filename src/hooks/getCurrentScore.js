61611a0e31a5d9c8d5e87de61dd42c288076b09f
j-preim <j.preimesberger@gmail.com>
2025-03-18 20:21:52 -0500

    currentscore

--- src/hooks/getCurrentScore.js
+++ src/hooks/getCurrentScore.js
@@ -1,15 +1,16 @@
 export const getCurrentScore = async (gameId) => {
   const apiKey = 'luKiaKnnuM5AyqYEimwuW2YnoRK9ks4F6wYPP307'
-  const scoreUrl = 'https://api.sportradar.us/ncaamb/trial/v8/en/games/' + gameId + 'boxscore.json?api_key=' + apiKey;
+  const scoreUrl = 'https://api.sportradar.us/ncaamb/trial/v8/en/games/' + gameId + '/boxscore.json?api_key=' + apiKey;
 
-  let currentScore;
+  let currentScore = [];
 
   await fetch(scoreUrl)
     .then(function (response) {
       return response.json();
     })
+    console.log(response)
     .then(function (data) {
-      currentScore = data.home.points;
+      currentScore = [data.home.points];
     })
     return currentScore;
 }
