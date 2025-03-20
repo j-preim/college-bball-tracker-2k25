46716df1aa548ac3a103b27811f43b935113e5a3
j-preim <j.preimesberger@gmail.com>
2025-03-19 20:06:26 -0500

    attempt 2

--- src/pages/api/schedule.js
+++
@@ -1,5 +0,0 @@
-import { schedule } from "sportradar";
-
-export default function handler(req, res) {
-     res.status(200).json({ schedule });
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,3 @@ export { default as Bracket } from "./Bracket";
 export { default as Matchups } from "./Matchups";
 export { default as Entries } from "./Entries";
 export { default as Auth } from "./Auth";
-
-const getSchedule = async () => {
-  const scheduleURL =
-    "https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3f-4024-47c4-900f-892883cc1b6b/schedule.json?api_key=luKiaKnnuM5AyqYEimwuW2YnoRK9ks4F6wYPP307";
-  const options = { method: "GET", headers: { accept: "application/json" } };
-
-  fetch(scheduleURL, options)
-    .then((res) => res.json())
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
-};
