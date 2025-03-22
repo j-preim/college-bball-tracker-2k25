68550ee8840671422ff5ec928f5150acd77e800b
j-preim <j.preimesberger@gmail.com>
2025-03-22 18:34:27 -0500

    ???

--- src/hooks/getInitSched.js
+++ src/hooks/getInitSched.js
@@ -1,5 +1,5 @@
 const apiKey = "luKiaKnnuM5AyqYEimwuW2YnoRK9ks4F6wYPP307";
-const apiUrl = `https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3f-4024-47c4-900f-892883cc1b6b/schedule.json?api_key=${apiKey}`;
+// const apiUrl = `https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3f-4024-47c4-900f-892883cc1b6b/schedule.json?api_key=${apiKey}`;
 
 // fetch(apiUrl)
 //   .then((response) => {
@@ -15,20 +15,19 @@ const apiUrl = `https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3
 //     console.error("Error:", error);
 //   });
 
+const apiUrl = `/test-cors-get`;
+
 export async function getData() {
   try {
     const response = await fetch(apiUrl, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
     });
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    console.log(json);
+    // const json = await response.json();
+    console.log(response);
   } catch (error) {
     console.error(error.message);
   }
