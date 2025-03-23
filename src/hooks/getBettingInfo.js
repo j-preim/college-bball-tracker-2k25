3bf02f1910d8239ac5f6b6fe5f63bbf06f9fb325
j-preim <j.preimesberger@gmail.com>
2025-03-23 13:48:06 -0500

    oops 2

--- src/hooks/getBettingInfo.js
+++ src/hooks/getBettingInfo.js
@@ -6,11 +6,11 @@ export async function getBettingInfo(gameId) {
   let bettingInfo = "TBD";
   let finalBettingInfo;
 
-  for (let i = 0; i < bettingData.sport_events.length; i++) {
-    if (bettingData.sport_events[i].uuids === gameId) {
-      bettingData.sport_events[i].status === "live" ? bettingInfo = bettingData.sport_events[i].consensus.lines[3].spread : bettingInfo = bettingData.sport_events[i].consensus.lines[2].spread
-    }
-  }
+  // for (let i = 0; i < bettingData.sport_events.length; i++) {
+  //   if (bettingData.sport_events[i].uuids === gameId) {
+  //     bettingData.sport_events[i].status === "live" ? bettingInfo = bettingData.sport_events[i].consensus.lines[3].spread : bettingInfo = bettingData.sport_events[i].consensus.lines[2].spread
+  //   }
+  // }
 
   
 
